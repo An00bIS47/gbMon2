42099ffb8cff669668482c9289973e657053e15d
michael <michael@macserver.local>
2013-10-21 14:57:54 +0200

    Initial Commit

---
+++ gbMon2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  gbMon2
+//
+//  Created by michael on 21.10.13.
+//  Copyright (c) 2013 michael. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
