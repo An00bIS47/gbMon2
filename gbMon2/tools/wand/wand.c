f7dd0b7828e42ae141b5add3574eff7be9bd3f91
michael <michael.hueber@gmail.com>
2014-03-04 15:35:41 +0100

    test

--- gbMon2/tools/wand/wand.c
+++ gbMon2/tools/wand/wand.c
@@ -29,14 +29,16 @@ int main() {
 	PixelSetColor(p_wand,"white");
 	m_wand = NewMagickWand();
 	// Create a 100x100 image with a default of white
-	MagickNewImage(m_wand,100,100,p_wand);
+	MagickNewImage(m_wand,128,64,p_wand);
+	
+	
 	// Get a new pixel iterator
 	iterator=NewPixelIterator(m_wand);
-	for(y=0;y<100;y++) {
+	for(y=0;y<64;y++) {
 		// Get the next row of the image as an array of PixelWands
 		pixels=PixelGetNextIteratorRow(iterator,&x);
 		// Set the row of wands to a simple gray scale gradient
-		for(x=0;x<100;x++) {
+		for(x=0;x<128;x++) {
 			gray = x*255/100;
 			sprintf(hex,"#%02x%02x%02x",gray,gray,gray);
 			PixelSetColor(pixels[x],hex);
@@ -44,6 +46,9 @@ int main() {
 		// Sync writes the pixels back to the m_wand
 		PixelSyncIterator(iterator);
 	}
+	
+	
+	
 	MagickWriteImage(m_wand,"bits_demo.gif");
 	
 	// Clean up
