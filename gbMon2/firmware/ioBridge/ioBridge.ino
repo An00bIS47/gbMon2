7496e32b05be91e132491520098acb9612ff0cd0
michael <michael.hueber@gmail.com>
2014-03-06 01:36:36 +0100

    ioBridge almost  finished

--- gbMon2/firmware/ioBridge/ioBridge.ino
+++ gbMon2/firmware/ioBridge/ioBridge.ino
@@ -1,37 +1,105 @@
+//
+//  ioBridge.ino
+//  gbmon2
+//
+//  Created by michael on 19.10.13.
+//  Copyright (c) 2013 michael. All rights reserved.
+//
+//   Pin	 Button		 Function
+//	=====	========	==========
+//	  7			0		  OK
+//	  6			1		  BACK
+//	  5			2		  DOWN
+//	  4			3		  UP
+//	  8			4		  LEFT
+//	  9			5		  RIGHT
+//
+//   Pin	 Part		 Function
+//	=====	========	==========
+//   A0	      LDR
+//   A1       EC
+//   A2       EC
+//   A3       EC
+//   A4       ??		  Can be used for any analog sensor
+//   A5       ??		  Can be used for any analog sensor
+//   A6       ??		  Can be used for any analog sensor
+//
+//   Pin	 Function
+//	=====	==========
+//   A0	      CLK
+//   A0	      DATA
+//
+//
+// The command for the Raspberry Pi:
+// !! The command will be send reversed !!
+//
+// |-----------|-------------------------------------------------------------------------|-----------|
+// |   FRAME   |                            INFORMATION									 |   FRAME   |
+// |-----------|-------------------------------------------------------------------------|-----------|
+// | 1110 0111 | 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 | 1110 0111 |
+// |-----------|-------------------------------------------------------------------------|-----------|
+// |	       |  Buttons   LDR       EC0      EC1      EC2      EC3      EC4      E5	 |			 |
+// |-----------|-------------------------------------------------------------------------|-----------|
+//
+// Binary:	10000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+// Hex:		80000000000000000000
+//
+//
+//
+// |-------------------------------------------------------------------------------------------------|
+// |										INFORMATION												 |
+// |-------------------------------------------------------------------------------------------------|
+// |																								 |
+// |			 |							Analog Values											 |
+// |			 |																					 |
+// |   Buttons   |    LDR    |    EC0    |    EC1    |    EC2    |    EC3    |    EC4    |    EC5	 |
+// | 0000	0000 | 0000 0000 | 0000 0000 | 0000 0000 | 0000 0000 | 0000 0000 | 0000 0000 | 0000 0000 |
+// | |||| ||||																						 |
+// | |||| ||||																						 |
+// | |||| |||Button 0																				 |
+// | |||| ||Button 1																				 |
+// | |||| |Button 2																					 |
+// | |||| Button 3																					 |
+// | |||Button 4																					 |
+// | ||Button 5																						 |
+// | |Short Click																					 |
+// | Long Click																						 |
+// |-------------------------------------------------------------------------------------------------|
+//
 
-/*
- *   Pin	 Button		 Function
- *	=====	========	==========
- *	  7			0		  Down
- *	  6			1		  Up
- *	  5			2		  Left
- *	  4			3		  Right
- *
- */
- 
-#define DEBOUNCE 10  // button debouncer, how many ms to debounce, 5+ ms is usually plenty
+//#include "DHT.h"
+
+//#define DHTPIN 11
+//#define DHTTYPE DHT22 //DHT11, DHT21, DHT22
+
+#define DEBOUNCE 10			// button debouncer, how many ms to debounce, 5+ ms is usually plenty
 #define LEDPIN 13
+#define CLKPIN	3			// D2
+#define DATAPIN	2			// D3
+#define NUMBERECSENSORS 6
+
+// This handy macro lets us determine how big the array up above is, by checking the size
+#define NUMBUTTONS sizeof(buttons)
 
 // here is where we define the buttons that we'll use. button "1" is the first, button "6" is the 6th, etc
 byte buttons[] = {9, 8, 7, 6, 5, 4}; // the analog 0-5 pins are also known as 14-19
-// This handy macro lets us determine how big the array up above is, by checking the size
-#define NUMBUTTONS sizeof(buttons)
+
 // we will track if a button is just pressed, just released, or 'currently pressed'
 volatile byte pressed[NUMBUTTONS], justpressed[NUMBUTTONS], justreleased[NUMBUTTONS];
 
-
-#include "DHT.h"
-
-#define DHTPIN 11
-#define DHTTYPE DHT22 //DHT11, DHT21, DHT22
-
-DHT dht(DHTPIN, DHTTYPE);
+//DHT dht(DHTPIN, DHTTYPE);
 
 long time[NUMBUTTONS];
 long lastUpdate=0;
 
-int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
-int photocellReading;     // the analog reading from the analog resistor divider
+int photocellPin = 0;					// the cell and 10K pulldown are connected to a0
+byte photocellReading;					// the analog reading from the ldr
+
+int ecPins[NUMBERECSENSORS] = {1,2,3,4,5,6};	// the ec level reading pins are connected to a1
+int ecReading[NUMBERECSENSORS];			// the analog reading from the ec probes
+
+int buttonValue	= 0;					// the button value
+int frame = 0;								// the command for the raspberry pi
 
 bool longClicked = false;
 
@@ -49,7 +117,12 @@ void setup() {
 	pinMode(LEDPIN, OUTPUT);
 
 	
-	dht.begin();
+	// DATA AND CLK TO OUTPUT
+	pinMode(CLKPIN, OUTPUT);
+	pinMode(DATAPIN, OUTPUT);
+	
+	buildFrame();
+	//dht.begin();
 	
 	// Make input & enable pull-up resistors on switch pins
 	for (i=0; i< NUMBUTTONS; i++) {
@@ -69,6 +142,91 @@ void setup() {
 
 SIGNAL(TIMER2_OVF_vect) {
 	check_switches();
+}
+
+
+void printByte(int value){
+	
+	for (unsigned int mask = 0x80; mask; mask >>= 1) {
+		if (mask & value) {
+			Serial.print('1');
+		}
+		else {
+			Serial.print('0');
+		}
+	}
+	//Serial.println();
+}
+
+void printCommand(){
+	Serial.println("|==========|=========================================================================|==========| ");
+    Serial.print("| ");
+	printByte(frame);
+	sendCommand(frame);
+    Serial.print(" | ");
+	
+	printByte(buttonValue);
+	sendCommand(buttonValue);
+	Serial.print(" ");
+	
+	printByte(photocellReading);
+	sendCommand(photocellReading);
+	Serial.print(" ");
+	
+	for (int i=0; i<NUMBERECSENSORS; i++) {
+		printByte(ecReading[i]);
+		sendCommand(ecReading[i]);
+		Serial.print(" ");
+	}
+    Serial.print("| ");
+	printByte(frame);
+	sendCommand(frame);
+	Serial.print(" |");
+	Serial.println();
+	Serial.println("|==========|=========================================================================|==========| ");
+}
+
+
+void sendCommand(unsigned int data){
+	/*
+	Serial.print("Data: ");
+	Serial.print(data, DEC);
+	Serial.print(" : ");
+	printByte(data);
+	Serial.print(" - ");
+	*/
+	for (unsigned int mask = 0x80; mask; mask >>= 1) {
+		digitalWrite(CLKPIN, HIGH);
+		if (mask & data) {
+			//Serial.print('1');
+			digitalWrite(DATAPIN, HIGH);
+		}
+		else {
+			//Serial.print('0');
+			digitalWrite(DATAPIN, LOW);
+		}
+		digitalWrite(CLKPIN, HIGH);
+		delay(3);
+	}
+	
+	//Serial.println();
+}
+
+void buildFrame(){
+	// Frame:	11100111
+	// Binary:	10000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+	// Decimal: 604462909807314587353088
+	// Binary:	11111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111
+	// Decimal: 1208925819614629174706175
+	//			79	  72 71	   64 63	56 55	 48	47	  40 39	   32 31	24 23 	 16	15	   8 7		0
+	// Hex:		80000000000000000000
+
+	frame |= (1 << 7);
+	frame |= (1 << 6);
+	frame |= (1 << 5);
+	frame |= (1 << 2);
+	frame |= (1 << 1);
+	frame |= (1 << 0);
 }
 
 void check_switches(){
@@ -119,13 +277,57 @@ void check_switches(){
 }
 
 void shortClick(int Button){
+	
+	buttonValue = 0;
+	buttonValue |= (1 << Button);
+	buttonValue |= (1 << 6);	// for short click
 	Serial.print(Button, DEC);
-	Serial.println(" pressed");
+	Serial.print(" pressed - Value: ");
+	
+	
+	// 0x8000 = 10000000 0000000
+	// 0x80   = 10000000
+	for (unsigned int mask = 0x80; mask; mask >>= 1) {
+		if (mask & buttonValue) {
+			Serial.print('1');
+		}
+		else {
+			Serial.print('0');
+		}
+	}
+	Serial.println();
+	
+	
+	// Print the command
+	printCommand();
+	
+	buttonValue = 0;
 }
 
 void longClick(int Button){
+	buttonValue = 0;
+	buttonValue |= (1 << Button);
+	buttonValue |= (1 << 7);	// for long click
 	Serial.print(Button, DEC);
-	Serial.println(" pressed LONG");
+	Serial.print(" pressed LONG - Value: ");
+	
+	
+	// 0x8000 = 10000000 0000000
+	// 0x80   = 10000000
+	for (unsigned int mask = 0x80; mask; mask >>= 1) {
+		if (mask & buttonValue) {
+			Serial.print('1');
+		}
+		else {
+			Serial.print('0');
+		}
+	}
+	Serial.println();
+	
+	// Print the command
+	printCommand();
+	
+	buttonValue = 0;
 }
 
 
@@ -133,44 +335,97 @@ void loop() {
 	
 	
 	photocellReading = analogRead(photocellPin);
+
+	for (int j=0; j < NUMBERECSENSORS; j++) {
+		ecReading[j] = analogRead(ecPins[j]);
+	}
+	
+	
 	if (lastUpdate == 0 ){
 		
-		Serial.print("Analog reading = ");
+		
+		for (int j=0; j < NUMBERECSENSORS; j++) {
+			
+			/*
+			Serial.print("ecLevel ");
+			Serial.print(j);
+			Serial.print(" reading = ");
+			Serial.print(ecReading[j]);
+			Serial.print(" : ");
+			
+			// 0x8000 = 10000000 0000000
+			// 0x80   = 10000000
+			for (unsigned int mask = 0x80; mask; mask >>= 1) {
+				if (mask & ecReading[j]) {
+					Serial.print('1');
+				}
+				else {
+					Serial.print('0');
+				}
+			}
+			Serial.println();
+			 */
+		}
+
+		/*
+		Serial.print("LDR reading = ");
 		Serial.print(photocellReading);
 
-				
+		
+		
 		// We'll have a few threshholds, qualitatively determined
 		if (photocellReading < 30) {
-			Serial.println(" - Dark");
+			Serial.print(" - Dark");
 		} else if (photocellReading < 150) {
-			Serial.println(" - Dim");
+			Serial.print(" - Dim");
 		} else if (photocellReading < 350) {
-			Serial.println(" - Light");
+			Serial.print(" - Light");
 		} else if (photocellReading < 550) {
-			Serial.println(" - Bright");
+			Serial.print(" - Bright");
 		} else {
-			Serial.println(" - Very bright");
+			Serial.print(" - Very bright");
 		}
 		
+		Serial.print(" : ");
 		
-		float h = dht.readHumidity();     //Luftfeuchte auslesen
-		float t = dht.readTemperature();  //Temperatur auslesen
-		
-		// Prüfen ob eine gültige Zahl zurückgegeben wird. Wenn NaN (not a number) zurückgegeben wird, dann Fehler ausgeben.
-		if (isnan(t) || isnan(h))
-		{
-			Serial.println("DHT22 konnte nicht ausgelesen werden");
+		// 0x8000 = 10000000 0000000
+		// 0x80   = 10000000
+		for (unsigned int mask = 0x80; mask; mask >>= 1) {
+			if (mask & photocellReading) {
+				Serial.print('1');
+			}
+			else {
+				Serial.print('0');
+			}
 		}
-		else
-		{
-			Serial.print("Luftfeuchte: ");
-			Serial.print(h);
-			Serial.print(" %\t");
-			Serial.print("Temperatur: ");
-			Serial.print(t);
-			Serial.println(" C");
-		}
+		Serial.println();
+		*/
 		
+		/*
+		 * Print AND Send the command
+		 */
+		printCommand();
+		
+		
+		/*
+		 float h = dht.readHumidity();     //Luftfeuchte auslesen
+		 float t = dht.readTemperature();  //Temperatur auslesen
+		 
+		 // Prüfen ob eine gültige Zahl zurückgegeben wird. Wenn NaN (not a number) zurückgegeben wird, dann Fehler ausgeben.
+		 if (isnan(t) || isnan(h))
+		 {
+		 Serial.println("DHT22 konnte nicht ausgelesen werden");
+		 }
+		 else
+		 {
+		 Serial.print("Luftfeuchte: ");
+		 Serial.print(h);
+		 Serial.print(" %\t");
+		 Serial.print("Temperatur: ");
+		 Serial.print(t);
+		 Serial.println(" C");
+		 }
+		 */
 		
 		lastUpdate=millis();
 	}
@@ -180,9 +435,6 @@ void loop() {
 	}
 	
 	
-	
-	
-
 	for (byte i = 0; i < NUMBUTTONS; i++) {
 		if (justpressed[i]) {
 			justpressed[i] = 0;
@@ -213,7 +465,6 @@ void loop() {
 			if((time[i] + 1000) < millis()){
 				
 				longClicked=true;
-				
 				
 			}
 			// is the button pressed down at this moment
