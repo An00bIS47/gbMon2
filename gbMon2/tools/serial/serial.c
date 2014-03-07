d10723a087f015468d5a63de3ec499eaaead271c
michael <michael.hueber@gmail.com>
2014-03-07 10:50:15 +0100

    added serial to tools

---
+++ gbMon2/tools/serial/serial.c
@@ -0,0 +1,31 @@
+//
+//  serial.c
+//  gbMon2
+//
+//  Created by michael on 07.03.14.
+//  Copyright (c) 2014 michael. All rights reserved.
+//
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <wiringSerial.h>
+
+int main (){
+	int fd ;
+	printf("Welcome to SerialRead\n");
+	if ((fd = serialOpen ("/dev/ttyAMA0", 115200)) < 0){
+		fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+		return 1 ;
+	}
+	
+	// Loop, getting and printing characters
+	
+	for (;;){
+		putchar (serialGetchar (fd)) ;
+		if (serialGetchar (fd)=='\n') {
+			printf("NEWLINE");
+		}
+		fflush (stdout) ;
+	}
+}
