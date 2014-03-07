d4e536580f99dc3ef61c59fe6f25d3b613c36816
michael <michael.hueber@gmail.com>
2014-03-07 12:01:21 +0100

    test

--- gbMon2/tools/ringbuffer/ringbuffer.c
+++ gbMon2/tools/ringbuffer/ringbuffer.c
@@ -1,319 +1,192 @@
-/* CIRCULAR BUFFER QUEUE */
-
+//<stdio.h> fuer printf()
+//<stdlib.h> fuer malloc()
+//<string.h> fuer strcpy()
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-
-/* data.h */
-/****************************************************************************************************/
-
-#define NumberOfTemperatureSensors 3
-#define NumberOfECSensors 3
-
-
-typedef struct {
-    float min;
-    float max;
-    float current;
-} Humidity;
-
-typedef struct {
-    float min;
-    float max;
-    float current;
-} EcLevel;
-
-typedef struct {
-    float min;
-    float max;
-    float current;
-} Temperature;
-
-typedef struct {
-    Temperature temperature[NumberOfTemperatureSensors];
-    Humidity humidity;
-    EcLevel ecLevel[NumberOfECSensors];
-    int lightValue;
-} Data;
-/****************************************************************************************************/
+//Struktur fuer die Daten
+typedef struct
+{
+    //Schluesselwert
+    int key;
+    //Name
+    char name[20];
+} userdata_t;
 
 //Struktur fuer einen Ringbuffer-Handler
 //wird benoetigt, um mehrere Listen zu verwalten
-typedef struct {
-    int readPointer;		//Index zum Lesen
-    int writePointer;		//Index zum Schreiben
-    Data *fifo;				//Platz fuer Speicherelemente, eigentlicher Buffer
-    int size;				//Groesse des Buffers, d.h. Anzahl der Elemente
-	int dataSize;			// number of chars in buffer
+typedef struct
+{
+    //Index zum Lesen
+    int readIndex;
+    //Index zum Schreiben
+    int writeIndex;
+    //Platz fuer Speicherelemente, eigentlicher Buffer
+    userdata_t *fifo;
+    //Groesse des Buffers, d.h. Anzahl der Elemente
+    int size;
 } ringbuffer_handler_t;
 
+/*-------------------------------------------------------------------------*/
 
-
-int input;              // user input
-
-
-/****************************************************************************************************/
-// prototypes
-int bufferIsFull(ringbuffer_handler_t *buffer);
-int bufferIsEmpty(ringbuffer_handler_t *buffer);
-void bufferPush(Data data, ringbuffer_handler_t *buffer);
-void bufferPop(ringbuffer_handler_t *buffer);
-ringbuffer_handler_t *bufferInit(int size);
-
-void bufferPush(Data data, ringbuffer_handler_t *buffer);
-/****************************************************************************************************/
-
-
-
-/****************************************************************************************************/
 //eine Funktion, um einen Ringbuffer anzulegen
 //Parameter size: Groesse des Buffers (Anzahl der Elemente)
 //Rückgabewert: Zeiger auf Ringbuffer-Handler
-ringbuffer_handler_t *bufferInit(int size) {
-	
+ringbuffer_handler_t *createFIFO(int size)
+{
     //Zeiger auf Ringbuffer-Handler deklarieren und genuegend Speicher
     //reservieren
-    ringbuffer_handler_t *buffer = (ringbuffer_handler_t *)malloc(sizeof(ringbuffer_handler_t));
+    ringbuffer_handler_t *buffer =
+	(ringbuffer_handler_t *)malloc(sizeof(ringbuffer_handler_t));
     
     //Werte des Handler belegen
     //readIndex und WriteIndex zu Anfang 0
     //fuer fifo muss genuegend Speicher reserviert werden,
     //um size Datenelemente aufnehmen zu koennen
     //size gibt Anzahl der Elemente im Ringbuffer an (aus Parameter)
-    buffer->readPointer=0;
-    buffer->writePointer=0;
-    buffer->fifo = (Data *)malloc(sizeof(Data) * (size + 1));
+    buffer->readIndex=0;
+    buffer->writeIndex=0;
+    buffer->fifo = (userdata_t *)malloc(sizeof(userdata_t) * (size + 1));
     buffer->size = size;
-	buffer->dataSize = 0;
 	
     //Zeiger auf Ringbuffer-Handler zurueckgeben
     return buffer;
 }
-/****************************************************************************************************/
 
-/****************************************************************************************************/
-int main(void) {
-	
-	int i;
-	int j;
-	
-	printf("Circular Buffer Queue Implementation");
-	
-	//eine Variable fuer die Daten
-    Data data;
+/*-------------------------------------------------------------------------*/
+
+//eine Funktion, um Elemente anzuhaengen, d.h. in den Buffer zu schreiben
+//Parameter data: Daten, die in den Buffer geschrieben werden sollen
+//Parameter *buffer: Zeiger auf Ringbuffer-Handler
+//kein Rueckgabewert
+void appendFIFO(userdata_t data, ringbuffer_handler_t *buffer)
+{
+    //Ringbuffer-Handler muss gueltig sein
+    if(buffer)
+    {
+		//Daten in den entsprechenden Platz im Buffer schreiben
+		//dafuer ist writeIndex da
+		buffer->fifo[buffer->writeIndex] = data;
+		//writeIndex wird fuer das naechste Schreiben auf den neuen Wert
+		//gesetzt
+		//Achtung: wenn der Buffer voll ist, wird wieder vorne hineinge-
+		//schrieben (deshalb RINGbuffer), Daten koennen verloren gehen,
+		//wenn nicht zwischendurch gelesen wird
+		buffer->writeIndex = buffer->writeIndex++ % (buffer->size+1);
+		//an dieser Stelle ist es deshalb sinnvoll, den readIndex auf den
+		//letzen wahren Wert zu setzen, wenn NICHT zwischendurch gelesen
+		//wurde
+		if(buffer->readIndex == buffer->writeIndex)
+			buffer->readIndex = buffer->readIndex ++ % (buffer->size+1);
+    }
+}
+
+/*-------------------------------------------------------------------------*/
+
+//eine Funktion, um Elemente aus dem Buffer zu lesen
+//Parameter *data: Zeiger auf ein Datenelement, das die gelesenen Daten
+//aufnehmen soll
+//Parameter *buffer: Zeiger auf Ringbuffer-Handler
+//Rueckgabewert -1, wenn nicht gelesen werden konnte
+//Rueckgabewert 1, wenn gelesen werden konnte
+int readFIFO(userdata_t *data, ringbuffer_handler_t *buffer)
+{
+    //Ringbuffer-Handler muss gueltig sein
+    if(buffer)
+    {
+		//der writeIndex zeigt immer auf das naechste zu beschreibende Element,
+		//d.h. dieses Element ist noch nicht beschrieben worden und enthaelt
+		//deshalb keine gueltigen Daten
+		//wenn readIndex also gleich writeIndex, darf nicht gelesen werden
+		if(buffer->readIndex != buffer->writeIndex)
+		{
+			//Daten kopieren
+			*data = buffer->fifo[buffer->readIndex];
+			//readIndex fuer das naechste Lesen hochsetzen
+			buffer->readIndex = buffer->readIndex++ % (buffer->size+1);
+			//Rueckgabewert 1, da gelesen wurde
+			return 1;
+		}
+		else
+			//es konnten keine Daten gelesen werden, da keine gueltigen Daten
+			//Rueckgabewert also -1
+			return -1;
+    }
+    //es konnte nicht gelesen werden, da Buffer-Handler nicht gueltig,
+    //Ruckegabewert also -1
+    else return -1;
+}
+
+/*-------------------------------------------------------------------------*/
+
+int main()
+{
+    //eine Variable fuer die Daten
+    userdata_t data;
     //ein Ringbuffer-Handler
     ringbuffer_handler_t *buffer;
     //eine Variable, um Ergebnisse von readFIFO() abzufragen
     int ergebnis;
 	
     //einen Ringbuffer-Handler und damit auch einen Ringbuffer anlegen
-    buffer = bufferInit(8);
+    buffer = createFIFO(2);
 	
-	// make sure there are no random chars in array, all spaces
-	//for (i = 0; i < buffer->size; i++) buffer->fifo[i] = 0x20;
+    //Daten anlegen und in den Buffer schreiben
+    data.key=10;
+    strcpy(data.name, "hans");
+    appendFIFO(data, buffer);
+    
+    //nochmal Daten anlegen und in den Buffer schreiben
+    data.key=11;
+    strcpy(data.name,"franz");
+    appendFIFO(data, buffer);
 	
-	printf("Fill Ringbuffer....\n");
+    //Daten aus Buffer holen, wenn erfolgreich, dann ausgeben
+    ergebnis = readFIFO(&data, buffer);
+    if(ergebnis == 1)
+    {
+		printf("%d %s\n", data.key, data.name);
+    }
 	
-	for (j=0; j<8; j++) {
-		
-		data.humidity.min=30.0;
-		data.humidity.max=50.0;
-		data.humidity.current=40.0;
-		
-		for (i=0; i<NumberOfTemperatureSensors; i++) {
-			data.temperature[i].min=19.0;
-			data.temperature[i].max=21.0;
-			data.temperature[i].current=20.0;
-		}
-		
-		for (i=0; i<NumberOfECSensors; i++) {
-			data.ecLevel[i].min=500;
-			data.ecLevel[i].max=700;
-			data.ecLevel[i].current=600;
-		}
-		bufferPush(data, buffer);
-	}
-
+    //Daten aus Buffer holen, wenn erfolgreich, dann ausgeben
+    ergebnis = readFIFO(&data, buffer);
+    if(ergebnis == 1)
+    {
+		printf("%d %s\n", data.key, data.name);
+    }
 	
-	/*
-	data.key=1;
-    strcpy(data.name,"alpha");
-	bufferPush(data, buffer);
-	data.key=2;
-    strcpy(data.name,"bravo");
-	bufferPush(data, buffer);
-	 */
+    //nochmal Daten anlegen und in den Buffer schreiben
+    data.key=12;
+    strcpy(data.name,"egon");
+    appendFIFO(data, buffer);
 	
-	while (input != 5) {
-		
-		printf("\n    press 1 to push char");
-		printf("\n    press 2 to pop char");	//pull_char
-		printf("\n    press 3 to show queue");
-		printf("\n    press 4 to bufferList");
-		printf("\n    press 5 to exit\n");
-		scanf("%d", &input);
-		
-		// push char
-		if (input == 1) {
-			
-			// Create fake data
-			data.humidity.min=30.0;
-			data.humidity.max=50.0;
-			data.humidity.current=40.0;
-			
-			for (i=0; i<NumberOfTemperatureSensors; i++) {
-				data.temperature[i].min=19.0;
-				data.temperature[i].max=21.0;
-				data.temperature[i].current=20.0;
-			}
-			
-			for (i=0; i<NumberOfECSensors; i++) {
-				data.ecLevel[i].min=500;
-				data.ecLevel[i].max=700;
-				data.ecLevel[i].current=600;
-			}
-			
-			if (! bufferIsFull(buffer)) {
-				bufferPush(data, buffer);
-			} else {
-				printf("\n!!! BUFFER IS FULL !!! ");
-				
-				// Pop "oldest" out
-				bufferPop(buffer);
-				// Push new in
-				bufferPush(data, buffer);
-			}
-				
-		}
-		// pull char
-		else if (input == 2) {
-			
-			if (! bufferIsEmpty(buffer))
-				bufferPop(buffer);
-			else
-				printf("\nBUFFER IS EMPTY!");
-		}
-		// display buffer info
-		else if (input == 3) {
-			
-			printf("\n   dataSize: %d readPointer: %d writePointer: %d",
-				   buffer->dataSize, buffer->readPointer, buffer->writePointer);
-			
-			printf("\n   Queue content:\n");
-			for (i = 0; i < buffer->size; i++) {
-				data = buffer->fifo[i];
-				printf("[%.1f]", data.temperature[0].current);
-			}
-			
-		}
-		
-		else if (input == 4) {
-			bufferList(buffer);
-		}
-		printf("\n----");
-	}
+    //Daten aus Buffer holen, wenn erfolgreich, dann ausgeben
+    ergebnis = readFIFO(&data, buffer);
+    if(ergebnis == 1)
+    {
+		printf("%d %s\n", data.key, data.name);
+    }
 	
-	return 0;
+    //jetzt einmal soviele Daten anlegen, dass Daten ueberschrieben werden
+    data.key=13;
+    strcpy(data.name, "walter");
+    appendFIFO(data, buffer);
+    data.key=14;
+    strcpy(data.name, "werner");
+    appendFIFO(data, buffer);
+    data.key=15;
+    strcpy(data.name, "wolfgang");
+    appendFIFO(data, buffer);
+    data.key=16;
+    strcpy(data.name, "felix");
+	
+    //Daten aus Buffer holen, wenn erfolgreich, dann ausgeben
+    //Der Datensatz (13, walter) ist verlorengegangen!), weil
+    //nicht oft genug gelesen wurde
+    ergebnis = readFIFO(&data, buffer);
+    if(ergebnis == 1)
+    {
+		printf("%d %s\n", data.key, data.name);
+    }
 }
-/****************************************************************************************************/
-
-
-/****************************************************************************************************/
-// adds a char
-void bufferPush(Data data, ringbuffer_handler_t *buffer) {
-	
-	if(buffer){
-		// increase write_pointer, check if at end of array
-		if (++buffer->writePointer >= buffer->size) {
-			buffer->writePointer = 0;
-		}
-	
-		//buffer[write_pointer] = c;
-		buffer->fifo[buffer->writePointer] = data;
-		
-		printf("\n--> PUSHED IN DATA AT INDEX: %d", buffer->writePointer);
-		buffer->dataSize++;
-	}
-}
-/****************************************************************************************************/
-
-
-/****************************************************************************************************/
-// returns 1 if buffer is full, 0 if buffer is not full, -1 if error
-int bufferIsFull(ringbuffer_handler_t *buffer) {
-	if (buffer) {
-		return buffer->readPointer == buffer->writePointer && buffer->dataSize == buffer->size;
-	} else
-		return -1;
-}
-/****************************************************************************************************/
-
-
-/****************************************************************************************************/
-// returns 1 if buffer is empty, 0 if buffer is not empty, -1 if error
-int bufferIsEmpty(ringbuffer_handler_t *buffer) {
-	if (buffer) {
-		return buffer->readPointer == buffer->writePointer && buffer->dataSize == 0;
-	} else
-		return -1;
-}
-/****************************************************************************************************/
-
-
-/****************************************************************************************************/
-// pull char from queue
-void bufferPop(ringbuffer_handler_t *buffer) {
-	if (++buffer->readPointer >= buffer->size) {
-			buffer->readPointer = 0;
-	}
-	
-	//printf("\nPopped char %c", buffer->fifo[buffer->readPointer]);
-	printf("\n--> POPPED OUT DATA AT INDEX: %d", buffer->readPointer);
-	
-	// enter space on place of read char so we can see it is removed
-	// buffer->fifo[buffer->readPointer]= 0x20;
-	
-	buffer->dataSize--;
-}
-/****************************************************************************************************/
-
-
-
-/****************************************************************************************************/
-void bufferList(ringbuffer_handler_t *buffer) {
-	//eine Variable fuer die Daten
-    Data data;
-	
-	int i,j;
-	printf("   dataSize: %d readPointer: %d writePointer: %d\n",
-			   buffer->dataSize, buffer->readPointer, buffer->writePointer);
-		
-	printf("   Queue content:\n");
-	for (j = 0; j < buffer->size; j++) {
-		data = buffer->fifo[j];
-		//printf("[%.1f]", data.temperature[0].current);
-		printf("===============================================\n");
-		printf("index: %d",j);
-		
-		printf("Humidity:\n");
-		printf("    min: %.1f\n", data.humidity.min);
-		printf("    max: %.1f\n", data.humidity.max);
-		printf("    current: %.1f\n", data.humidity.current);
-
-		for (i=0; i<NumberOfTemperatureSensors; i++) {
-			printf("Temperature[%d]:\n",i);
-			printf("    min: %.1f\n", data.temperature[i].min);
-			printf("    max: %.1f\n", data.temperature[i].max);
-			printf("    current: %.1f\n", data.temperature[i].current);
-		}
-
-		for (i=0; i<NumberOfECSensors; i++) {
-			printf("ec-Level[%d]:\n",i);
-			printf("    min: %.1f\n", data.ecLevel[i].min);
-			printf("    max: %.1f\n", data.ecLevel[i].max);
-			printf("    current: %.1f\n", data.ecLevel[i].current);
-		}
-	
-	}
-}
-/****************************************************************************************************/
-
--- gbMon2/tools/serial/serial.c
+++ gbMon2/tools/serial/serial.c
@@ -8,9 +8,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <stdlib.h>
 
 #include <wiringSerial.h>
 #define NOECSENSORS 3						// Number of EC Sensors
+
+//Struktur fuer die Daten
+typedef struct
+{
+    //Schluesselwert
+    int key;
+    //Name
+    char inChar;
+} userdata_t;
+
+//Struktur fuer einen Ringbuffer-Handler
+//wird benoetigt, um mehrere Listen zu verwalten
+typedef struct
+{
+    //Index zum Lesen
+    int readIndex;
+    //Index zum Schreiben
+    int writeIndex;
+    //Platz fuer Speicherelemente, eigentlicher Buffer
+    userdata_t *fifo;
+    //Groesse des Buffers, d.h. Anzahl der Elemente
+    int size;
+} ringbuffer_handler_t;
+
 
 /*
  * Substring
@@ -23,8 +48,111 @@ char* substring(const char* str, size_t begin, size_t len){
     return strndup(str + begin, len);
 }
 
+
+//eine Funktion, um einen Ringbuffer anzulegen
+//Parameter size: Groesse des Buffers (Anzahl der Elemente)
+//Rückgabewert: Zeiger auf Ringbuffer-Handler
+ringbuffer_handler_t *createFIFO(int size)
+{
+    //Zeiger auf Ringbuffer-Handler deklarieren und genuegend Speicher
+    //reservieren
+    ringbuffer_handler_t *buffer =
+	(ringbuffer_handler_t *)malloc(sizeof(ringbuffer_handler_t));
+    
+    //Werte des Handler belegen
+    //readIndex und WriteIndex zu Anfang 0
+    //fuer fifo muss genuegend Speicher reserviert werden,
+    //um size Datenelemente aufnehmen zu koennen
+    //size gibt Anzahl der Elemente im Ringbuffer an (aus Parameter)
+    buffer->readIndex=0;
+    buffer->writeIndex=0;
+    buffer->fifo = (userdata_t *)malloc(sizeof(userdata_t) * (size + 1));
+    buffer->size = size;
+	
+    //Zeiger auf Ringbuffer-Handler zurueckgeben
+    return buffer;
+}
+
+/*-------------------------------------------------------------------------*/
+
+//eine Funktion, um Elemente anzuhaengen, d.h. in den Buffer zu schreiben
+//Parameter data: Daten, die in den Buffer geschrieben werden sollen
+//Parameter *buffer: Zeiger auf Ringbuffer-Handler
+//kein Rueckgabewert
+void appendFIFO(userdata_t data, ringbuffer_handler_t *buffer)
+{
+    //Ringbuffer-Handler muss gueltig sein
+    if(buffer)
+    {
+		//Daten in den entsprechenden Platz im Buffer schreiben
+		//dafuer ist writeIndex da
+		buffer->fifo[buffer->writeIndex] = data;
+		//writeIndex wird fuer das naechste Schreiben auf den neuen Wert
+		//gesetzt
+		//Achtung: wenn der Buffer voll ist, wird wieder vorne hineinge-
+		//schrieben (deshalb RINGbuffer), Daten koennen verloren gehen,
+		//wenn nicht zwischendurch gelesen wird
+		buffer->writeIndex = buffer->writeIndex++ % (buffer->size+1);
+		//an dieser Stelle ist es deshalb sinnvoll, den readIndex auf den
+		//letzen wahren Wert zu setzen, wenn NICHT zwischendurch gelesen
+		//wurde
+		if(buffer->readIndex == buffer->writeIndex)
+			buffer->readIndex = buffer->readIndex ++ % (buffer->size+1);
+    }
+}
+
+/*-------------------------------------------------------------------------*/
+
+//eine Funktion, um Elemente aus dem Buffer zu lesen
+//Parameter *data: Zeiger auf ein Datenelement, das die gelesenen Daten
+//aufnehmen soll
+//Parameter *buffer: Zeiger auf Ringbuffer-Handler
+//Rueckgabewert -1, wenn nicht gelesen werden konnte
+//Rueckgabewert 1, wenn gelesen werden konnte
+int readFIFO(userdata_t *data, ringbuffer_handler_t *buffer)
+{
+    //Ringbuffer-Handler muss gueltig sein
+    if(buffer)
+    {
+		//der writeIndex zeigt immer auf das naechste zu beschreibende Element,
+		//d.h. dieses Element ist noch nicht beschrieben worden und enthaelt
+		//deshalb keine gueltigen Daten
+		//wenn readIndex also gleich writeIndex, darf nicht gelesen werden
+		if(buffer->readIndex != buffer->writeIndex)
+		{
+			//Daten kopieren
+			*data = buffer->fifo[buffer->readIndex];
+			//readIndex fuer das naechste Lesen hochsetzen
+			buffer->readIndex = buffer->readIndex++ % (buffer->size+1);
+			//Rueckgabewert 1, da gelesen wurde
+			return 1;
+		}
+		else
+			//es konnten keine Daten gelesen werden, da keine gueltigen Daten
+			//Rueckgabewert also -1
+			return -1;
+    }
+    //es konnte nicht gelesen werden, da Buffer-Handler nicht gueltig,
+    //Ruckegabewert also -1
+    else return -1;
+}
+
+
 int main (){
 	int fd ;
+	
+	//eine Variable fuer die Daten
+    userdata_t data;
+    //ein Ringbuffer-Handler
+    ringbuffer_handler_t *buffer;
+    //eine Variable, um Ergebnisse von readFIFO() abzufragen
+    int ergebnis;
+	
+    //einen Ringbuffer-Handler und damit auch einen Ringbuffer anlegen
+    buffer = createFIFO(255);
+	
+
+	
 	
 	char inData[255];	// Allocate some space for the string
 	char inChar = -1;	// Where to store the character read
@@ -45,18 +173,32 @@ int main (){
 	for (;;) {
 		while (serialDataAvail(fd)) {
 			inChar = (serialGetchar(fd));
-			inData[spos]= inChar;
+			
+			//Daten anlegen und in den Buffer schreiben
+			data.key=spos;
+			strcpy(&data.inChar, &inChar);
+			appendFIFO(data, buffer);
+			
+			//inData[spos]= inChar;
 			spos++;
-
+			
+			//Daten aus Buffer holen, wenn erfolgreich, dann ausgeben
+			ergebnis = readFIFO(&data, buffer);
+			if(ergebnis == 1)
+			{
+				printf("%d %c\n", data.key, data.inChar);
+			}
+			
+			/*
 			//inData[spos]= '\0';
 			if (inChar == 10) {			// LF 10 ; CR 13
 				//printf("%s\n",inData);
 				
-				/*
-				if (strncmp(inData,"11100111",8) == 0) {
-					printf("*** FRAME ***\n");
-				}
-				*/
+				
+				//if (strncmp(inData,"11100111",8) == 0) {
+				//	printf("*** FRAME ***\n");
+				//}
+				
 				printf("Buttons:\t%s\n", substring(inData,curPos,8));
 				curPos=curPos+8;
 				printf("LDR:\t%s\n", substring(inData,16,8));
@@ -66,17 +208,18 @@ int main (){
 					printf("EC %d:\t%s\n",i, substring(inData,curPos,8));
 					curPos=curPos+8;
 				}
-				/*
-				if (strcmp(substring(inData,curPos,8),"11100111") == 0) {
-					printf("*** FRAME ***\n");
-				}
-				*/
+				
+				//if (strcmp(substring(inData,curPos,8),"11100111") == 0) {
+				//	printf("*** FRAME ***\n");
+				//}
+				
 				spos = 0;
 				//inData[spos]='\0';
 				
 				//fflush (stdout) ;
 			} // if (inChare == 13
-
+			 */
+			
 		} // while
 	} //for
 }
