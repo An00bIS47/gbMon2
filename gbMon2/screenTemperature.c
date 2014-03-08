d4b7dcee5f904af040246666c0a2db29cc15431f
michael <michael.hueber@gmail.com>
2014-03-08 22:34:05 +0100

    test

--- gbMon2/screenTemperature.c
+++ gbMon2/screenTemperature.c
@@ -14,7 +14,31 @@ void displayBorder(){
 
 	
 	// Vertikale Linie
-	GLCDD_Line(10,10,10,60);
+	GLCDD_Line(10,20,10,60);
+	
+	// 40
+	GLCDD_Line(6,20,10,20);
+	
+	// 35
+	GLCDD_Line(8,25,10,25);
+	
+	// 30
+	GLCDD_Line(6,30,10,30);
+	
+	// 25
+	GLCDD_Line(8,35,10,35);
+	
+	// 20
+	GLCDD_Line(6,40,10,40);
+
+	// 15
+	GLCDD_Line(8,45,10,45);
+	
+	// 10
+	GLCDD_Line(6,50,10,50);
+	
+	// 5
+	GLCDD_Line(8,55,10,55);
 	
 	// Horizontale Linie
 	GLCDD_Line(10,60,118,60);
