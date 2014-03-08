e8a90e82037ca6a248db1ae9c5df439d424bc05d
michael <michael.hueber@gmail.com>
2014-03-08 13:38:51 +0100

    test

--- gbMon2/ioBridge.c
+++ gbMon2/ioBridge.c
@@ -41,7 +41,7 @@ void* ioBridgeMain (void *args){
 				printf("%s",inData);
 				
 				strcpy(startFrame,substring(inData,curPos,8));
-				printf("StartFrame:		%s\n",startFrame);
+				printf("StartFrame:	  %s\n",startFrame);
 				curPos=curPos+8;
 				
 				strcpy(buttons,substring(inData,curPos,8));
@@ -63,7 +63,7 @@ void* ioBridgeMain (void *args){
 				printf("endFrame:	%s\n",endFrame);
 				curPos=0;
 				
-				if (strcmp(endFrame, "11100111")) {
+				if (strcmp(endFrame, "11100111") == 0) {
 					printf("HERE!\n\n");
 				}
 				
