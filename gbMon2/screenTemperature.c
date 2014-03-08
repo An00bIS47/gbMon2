93faabfbfe9f82015218a866ff3b61d8e35765ca
michael <michael.hueber@gmail.com>
2014-03-08 22:04:43 +0100

    test screenTemperature

---
+++ gbMon2/screenTemperature.c
@@ -0,0 +1,16 @@
+//
+//  screenTemperature.c
+//  gbMon2
+//
+//  Created by michael on 08.03.14.
+//  Copyright (c) 2014 michael. All rights reserved.
+//
+
+#include "screenTemperature.h"
+
+
+void displayBorder(){
+	//GLCDD_Line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2)
+	GLCDD_Line(5,5,10,5);
+	
+}
---
+++ gbMon2/screenTemperature.h
@@ -0,0 +1,15 @@
+//
+//  screenTemperature.h
+//  gbMon2
+//
+//  Created by michael on 08.03.14.
+//  Copyright (c) 2014 michael. All rights reserved.
+//
+
+#ifndef gbMon2_screenTemperature_h
+#define gbMon2_screenTemperature_h
+
+#include <stdio.h>
+#include "lcd.h"
+
+#endif
